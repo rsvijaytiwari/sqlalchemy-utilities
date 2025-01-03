f113a27eb043242a074508fddc6a717ced4c405a
Vijay Tiwari <rsvijaytiwari@gmail.com>
2025-01-03 13:48:34 +0530

    Update package version and correct package inclusion
    Bump version from 1.0.1 to 1.0.2 to reflect changes. Fixed `find_packages` to correctly include `sqlalchemy-utilities` instead of `utilities`.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='sqlalchemy-utilities',
-    version='1.0.1',
+    version='1.0.2',
     description='Included sqlalchemy serializers',
     author='Vijay Tiwari',
     author_email='rsvijaytiwari@gmail.com',
-    packages=find_packages(include=['utilities', 'utilities.*']),
+    packages=find_packages(include=['sqlalchemy-utilities', 'sqlalchemy-utilities.*']),
     install_requires=['sqlalchemy']
 )
