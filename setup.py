165f3475f82f74852b73fdc330d0bd10d2d96bf9
Vijay Tiwari <rsvijaytiwari@gmail.com>
2025-01-03 13:15:38 +0530

    Simplify dependency handling in setup.py
    Replaced the custom `parse_requirements` function with a hardcoded dependency list. This reduces complexity and removes the need to parse an external requirements file during installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup, find_packages
 
 
-def parse_requirements(filename):
-    with open(filename, "r", encoding="utf-8") as f:
-        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
-
-
 setup(
     name='sqlalchemy-utilities',
     version='1.0.0',
@@ -13,5 +8,5 @@ setup(
     author='Vijay Tiwari',
     author_email='rsvijaytiwari@gmail.com',
     packages=find_packages(include=['utilities', 'utilities.*']),
-    install_requires=parse_requirements("requirements.txt")
+    install_requires=['sqlalchemy']
 )
